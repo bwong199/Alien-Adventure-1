b38872c4d21a8896ee04a56660dff196988dcd3d
bwong199 <bwong199@my.bcit.ca>
2016-09-09 23:07:53 -0600

    Fixed last question

--- Alien Adventure/RemoveDuplicates.swift
+++ Alien Adventure/RemoveDuplicates.swift
@@ -12,17 +12,12 @@ extension Hero {
         var withoutDuplicates = [UDItem]()
         
         for item in inventory {
-            for uniqueItem in withoutDuplicates {
-                var value = false
-                if item == uniqueItem {
-                    value = value || true
-                }
-                if value == false {
-                    withoutDuplicates.append(item)
-                }
+            if !withoutDuplicates.contains(item){
+                withoutDuplicates.append(item)
             }
         }
         
-        return withoutDuplicates    }
+        return withoutDuplicates
+    }
     
 }
